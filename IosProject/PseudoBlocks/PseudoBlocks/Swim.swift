22c9184d4b7a8063145e400abeedd7757a9b697a
FroZnKiller <fhict@MacBook.local>
2015-04-16 14:35:48 +0200

    accelerometer added not tested

--- IosProject/PseudoBlocks/PseudoBlocks/Swim.swift
+++ IosProject/PseudoBlocks/PseudoBlocks/Swim.swift
@@ -7,11 +7,63 @@
 //
 
 import Foundation
-
+import CoreMotion
 
 class Swim: Block {
     
+    var motionManager = CMMotionManager()
+    
+    var curMaxAccelX: Double = 0.0
+    var curMaxAccelY: Double = 0.0
+    var curMaxAccelZ: Double = 0.0
+    
+    
     func start() {
-        // todo
+        // set motion maneger propperty
+        motionManager.accelerometerUpdateInterval = 0.2
+        
+        ResetMaxAccel()
+        
+        //start receiving data
+        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue(),
+            withHandler: { (accelerometerData: CMAccelerometerData!, error: NSError!) -> Void
+                in
+                self.outputAccelerationData(accelerometerData.acceleration)
+                if (error != nil){
+                    println("\(error)")
+                }
+            })
+        
+    }
+    
+    func stop()
+    {
+        ResetMaxAccel()
+    }
+    
+    func ResetMaxAccel()
+    {
+         curMaxAccelX = 0
+         curMaxAccelY = 0
+         curMaxAccelZ = 0
+    }
+    
+    func outputAccelerationData(acceleration: CMAcceleration){
+        // set x acceration
+        if fabs(acceleration.x) > fabs(curMaxAccelX){
+            curMaxAccelX = acceleration.x
+        }
+        
+        // set y acceration
+        if fabs(acceleration.y) > fabs(curMaxAccelY){
+            curMaxAccelY = acceleration.y
+        }
+        
+        // set x acceration
+        if fabs(acceleration.z) > fabs(curMaxAccelZ){
+            curMaxAccelZ = acceleration.z
+        }
+        
+        
     }
 }
